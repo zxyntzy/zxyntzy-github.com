fd79202f40642e9b060d7fc886689bec06ba1eef
zxyntzy <zxyntzy@gmail.com>
2024-10-13 20:14:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
